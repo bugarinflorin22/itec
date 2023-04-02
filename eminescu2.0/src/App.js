e983ff1a004b328805c11fe8a4e73fac3b1f5afb
bugarinflorin22 <bugarinflorin@gmail.com>
2023-04-02 11:49:27 +0300

    sdas

--- eminescu2.0/src/App.js
+++ eminescu2.0/src/App.js
@@ -1,34 +1,64 @@
 import './App.css';
 import React, {useState} from 'react';
+import nothing from './img/Untitled.png';
+import emi from './img/Eminescu.jpg'
+import bubbles from './img/bubble.png';
+import black from './img/black.png';
+
 
 function App() {
-  const [backEndData, setBackendData] = useState('');
+  const [inputPrompt, setInputPrompt] = useState('');
+  const [poemGenerate, setPoemGenerate] = useState('');
+  const [imageGenerator, setImageGenerator] = useState('');
+
+  if (imageGenerator == '') {
+    setImageGenerator(nothing);
+  }
 
   const handleSubmit = async (e) => {
+    if (inputPrompt == '') inputPrompt = "itec";
     e.preventDefault();
-    fetch('http://localhost:1616/api', {
+    fetch('http://localhost:1616/poemGenerator', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        message: backEndData,
+        promptFromUser: inputPrompt,
       }),
     })
-    .then((res) => res.json())
-    .then((data) => setBackendData(data.message))
+    .then((dataFromAI) => dataFromAI.json())
+    .then((data) => setPoemGenerate(data.poemFromAI));
+
+    fetch('http://localhost:1616/imageGenerator', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        promptFromUser: inputPrompt,
+      }),
+    })
+    .then((dataFromAI) => dataFromAI.json())
+    .then((data) => setImageGenerator(data.imageFromAI));
   };
 
-  return (
-  <div className="App">
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="message">Message</label>
-      <textarea id="message" value={backEndData} onChange={(e) => setBackendData(e.target.value)} />
-      <button type="submit">Submit</button>
-    </form>
-    <div>{backEndData}</div>
-  </div>
-  );
+    return (
+      <div className="App">
+        <div className="main">
+          <div className="textgen">
+            <pre> {poemGenerate} </pre>
+            <img className='img_gen' src={imageGenerator} alt="img generate" />
+          </div> 
+          <div className='emiandhesmind'>
+            <h1> Eminescu 2.0 </h1>
+            <img src={emi} className="emi"></img>
+            <textarea placeholder="Ex: cars" className="prompt" id="message" onChange={(e) => setInputPrompt(e.target.value)} />
+            <p onClick= {handleSubmit} className="btn" type="submit" value="Submit"> Generate </p>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,17 +1,53 @@
+require("dotenv").config();
+
+const OpenAi = require("openai");
+const { Configuration, OpenAIApi } = OpenAi;
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = 1616;
+const port = parseInt(process.env.REACT_APP_PORT);
+
+const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_API_KEY
+});
+
+var poem = ""
+
+const openai = new OpenAIApi(configuration);
     
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/api', (rq, res) => {
-    res.json({ 
-        message: "Hello from server!"
+app.post('/poemGenerator', async (dataFromUser, dataFromAI) => {
+    const {promptFromUser} = dataFromUser.body;
+
+    const response = await openai.createCompletion({
+        model: process.env.REACT_APP_MODEL_AI,
+        prompt: process.env.REACT_APP_PROMPT + promptFromUser,
+        temperature: parseInt(process.env.REACT_APP_TEMPERATURE),
+        max_tokens: parseInt(process.env.REACT_APP_MAX_TOKENS),
     });
+
+    dataFromAI.json({ 
+        poemFromAI: response.data.choices[0].text
+    }); 
+    poem = response.data.choices[0].text;
+});
+
+app.post('/imageGenerator', async (dataFromUser, dataFromAI) => {
+    const {promptFromUser} = dataFromUser.body;
+
+    const response = await openai.createImage({
+        prompt: promptFromUser,
+        n: 1,
+        size: process.env.REACT_APP_IMG_SIZE,
+    });
+
+    dataFromAI.json({ 
+        imageFromAI: response.data.data[0].url
+    }); 
 });
 
 app.listen(port);
